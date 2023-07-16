7265e57116bca20ce6f6bec1d3a10aa94e0f9b2e
AshishKrtimaIQ <131434605+AshishKrtimaIQ@users.noreply.github.com>
2023-07-16 14:24:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express =  require('express');
+const colors = require('colors');
+const dotenv = require('dotenv');
+const moragan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const app = express()
+
+app.use(express.json());
+app.use(moragan("dev"));
+
+app.get("/", (req,res) => {
+    res.status(200).send({
+        message: "server running",
+
+    });
+});
+
+const port = process.env.PORT || 8080
+
+app.listen (port, () => {
+    console.log(
+        `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}` .bgCyan.white
+);
+ });
